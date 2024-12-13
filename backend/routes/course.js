34dc2265f9fbaa67e5fea50eb4503e9384ae5782
2aurora2 <1971261337@qq.com>
2024-12-13 21:37:07 +0800

    backend router config

---
+++ backend/routes/course.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+const responseUtil = require("../utils/response");
+
+router.get('/get/info', async (req, res, next) => {
+    responseUtil.success(res, {
+        name: '数据库系统实现'
+    });
+});
