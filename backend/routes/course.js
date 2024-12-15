ed85f1e3f1fea314c96aff532a76a5742f302b41
WA_automat <1577696824@qq.com>
2024-12-16 01:34:12 +0800

    课程CRUD接口

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -46,4 +46,64 @@ router.post('/create', async (req, res, next) => {
     }
 });
 
+router.post('/delete', async (req, res, next) => {
+    let course_id = req.body.course_id;
+    var connection = await oracledb.getConnection();
+    try {
+        // 调用 PL/SQL 执行
+        const result = await connection.execute(
+            `BEGIN :flag := delete_course_by_id(:course_id); END;`,
+            {
+                course_id: {val: course_id, dir: oracledb.BIND_IN, type: oracledb.NUMBER},
+                flag: {dir: oracledb.BIND_OUT, type: oracledb.NUMBER}
+            }
+        );
+        if (result.outBinds.flag === -1) {
+            console.log("课程不存在");
+        }
+        responseUtil.success(res, {});
+    } catch (e) {
+        console.error(e);
+        responseUtil.error(res);
+    } finally {
+        if (connection) {
+            await connection.close();
+        }
+    }
+});
+
+router.get('/get', async (req, res, next) => {
+    const {name} = req.query;
+    var connection = await oracledb.getConnection();
+    try {
+        // 调用PL/SQL函数
+        const result = await connection.execute(
+            `BEGIN :result := get_course_class_count(${name ? `'${name}'` : 'NULL'}); END;`,
+            {
+                result: { type: oracledb.CURSOR, dir: oracledb.BIND_OUT }
+            }
+        );
+        // 处理游标结果
+        const cursor = result.outBinds.result;
+
+        // 读取游标结果
+        let courseList = []
+        let row;
+        while ((row = await cursor.getRow())) {
+            courseList.push(row)
+        }
+
+        responseUtil.success(res, {
+            courseList: courseList
+        });
+    } catch (e) {
+        console.error(e);
+        responseUtil.error(res);
+    } finally {
+        if (connection) {
+            await connection.close();
+        }
+    }
+});
+
 module.exports = router;
